99aa66085797dd03ea7ccec568ddc9c1b6177589
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-04-08 22:13:40 -0700

    added the ability to store user data in firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,6 +12,30 @@ const config = {
     appId: "1:715851670622:web:50795a5b92ca2875ed00cf"
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if(!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists) {
+      const {displayName, email} = userAuth;
+      const createdAt = new Date();
+
+      try {
+          await userRef.set({
+              displayName,
+              email,
+              createdAt,
+              ...additionalData
+          })
+      } catch (err) {
+          console.log('error creating user', err.message)
+      }
+  }
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
