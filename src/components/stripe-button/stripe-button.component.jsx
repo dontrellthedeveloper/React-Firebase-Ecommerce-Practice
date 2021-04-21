5e3782163b80c6b5397c7bbcd46c4627fd84f3a8
Dontrell K. Washington <dontrellthedeveloper@hotmail.com>
2021-04-20 23:58:18 -0700

    added stripe integration

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({price}) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51IiZmvJJnrCDMTtdotQgiJcV9JuZN0wZvE1Opgbr3R42nBTXanvUb7sKBFNfh69pTxxAb8evuBmzonVDImLPTXTJ00gWA31BZp';
+
+  const onToken = token => {
+      console.log(token)
+      alert('Payment Successful')
+  };
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='CRNN Clothing Ltd'
+            billingAddress
+            shippingAddress
+            image
+            description={`Your total price is $${price}`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+
+        />
+    )
+};
+
+export default StripeCheckoutButton;
